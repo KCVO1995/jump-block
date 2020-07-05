d5e0254c3791233cd95a75e2a0a9c587213b0b4c
kcvo <943514589@qq.com>
2020-07-05 19:44:22 +0800

    init

---
+++ src/components/JumpBlock.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="global">
+    <ul class="container">
+      <li v-for="count in itemCount" :key="count" class="item"></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop({ default: 9 }) readonly rowCount?: number
+  @Prop({ default: 9 }) readonly colCount?: number
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .global {
+    width: 100vw;
+    height: 100vh;
+    > .container {
+      width: 100%;
+      height: 100%;
+      border: 1px solid blue;
+      display: flex;
+      flex-wrap: wrap;
+      > .item {
+        border: 2px solid red;
+      }
+    }
+  }
+</style>
