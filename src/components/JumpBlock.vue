e38486de799703bd95dfb968ccb2fb85ca630140
KCVO1995 <943514589@qq.com>
2020-07-05 23:06:14 +0800

    Fix: count duplicate problem

--- src/components/JumpBlock.vue
+++ src/components/JumpBlock.vue
@@ -2,7 +2,8 @@
   <div class="global">
     <div class="container">
       <div v-for="colCount in col" :key="colCount" class="col-item" :style="{marginBottom: gap + 'px'}">
-        <div v-for="rowCount in row" :key="rowCount" class="row-item" :id="(colCount - 1) * row + rowCount" :style="{marginRight: gap + 'px'}" ></div>
+        <div v-for="rowCount in row" :key="rowCount" class="row-item" :id="(colCount - 1) * row + rowCount"
+             :style="{marginRight: gap + 'px'}"></div>
       </div>
     </div>
   </div>
@@ -16,7 +17,7 @@
     currentCount = '';
     row = 3;
     col = 3;
-    duration = 500;
+    duration = 1000;
     changeBackground = true;
     defaultBackground = 'white';
     colorRange_R = [0, 100];
@@ -40,21 +41,25 @@
 
     @Watch('currentCount')
     onChildChanged(val: string, oldVal: string) {
-      if (oldVal === val) return this.getRandom(1, this.total, true);
-      console.log('count', val, 'oldCount', oldVal, 'boolean', val === oldVal);
-      const old = document.getElementById(oldVal);
-      const current = document.getElementById(val);
+      const oldCount = oldVal.split('T')[0];
+      const newCount = val.split('T')[0];
+      if (oldCount === newCount) {
+        this.getRandom(1, this.total, true);
+        return;
+      }
+      const old = document.getElementById(oldCount);
+      const current = document.getElementById(newCount);
       if (old) old.innerHTML = '';
       if (current) current.appendChild(this.createJumpBlock());
     }
 
     mounted() {
-      setInterval(() => this.getRandom(1, this.total, true), this.duration);
+      setInterval(() => {this.getRandom(1, this.total, true);}, this.duration);
     }
 
     getRandom(min: number, max: number, jump = false) {
       const random = (Math.floor(Math.random() * Math.floor(max - min + 1)) + min).toString();
-      return jump ? this.currentCount = random : random;
+      return jump ? this.currentCount = random + 'T' + Math.random() : random;
     }
 
     getColor() {
